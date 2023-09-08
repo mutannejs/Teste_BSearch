37cb53b43834a817c0bc9986dd2790097ce810d5
mutannejs <murillojustinoklein@gmail.com>
2023-09-07 22:36:49 -0300

    cmp em outro arquivo

--- main.c
+++ main.c
@@ -1,13 +1,5 @@
 #include "binary_search.h"
 
-int cmp(const void *num1, const void *num2) {
-	if ( (*((const lli *)num1)) > (*((const lli *)num2)) )
-		return 1;
-	if ( (*((const lli *)num1)) < (*((const lli *)num2)) )
-		return -1;
-	return 0;
-}
-
 int main(int argc, char *argv[]) {
 
 	int istop = 1;
