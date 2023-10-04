033f4f70996dcf44ec83e6d15078f206d5d27cd0
mutannejs <murillojustinoklein@gmail.com>
2023-10-03 21:04:49 -0300

    binary_search.c adicionado

---
+++ binary_search.c
@@ -0,0 +1,7 @@
+#include "src/binary_search.h"
+
+void* binary_search(const void *key, const void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *), int *path, int ifnotfound) {
+
+	// INSIRA O CÓDIGO DA SUA BUSCA BINÁRIA AQUI
+	
+}
